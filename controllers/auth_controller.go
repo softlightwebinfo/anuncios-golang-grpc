150db0c57a1983e3de348c4e581cb64b02f1e083
Rafael González Muñoz <rafael.gonzalez.1737@gmail.com>
2020-11-29 16:26:38 +0100

    AN-13: Generar nuevo token para modificar la contraseña y actualizar la contraseña

--- controllers/auth_controller.go
+++ controllers/auth_controller.go
@@ -2,12 +2,15 @@ package controllers
 
 import (
 	"cientosdeanuncios.com/backend/enums"
+	"cientosdeanuncios.com/backend/libs"
 	"cientosdeanuncios.com/backend/models"
 	"cientosdeanuncios.com/backend/proto"
 	"context"
 	"database/sql"
+	"fmt"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+	"time"
 )
 
 type AuthController struct {
@@ -101,5 +104,68 @@ func (then AuthController) Register(_ context.Context, rq *proto.AuthRegisterReq
 	response.User = &user
 	response.Token = token
 
+	return &response, nil
+}
+func (then AuthController) SendRecoveryAccount(_ context.Context, rq *proto.AuthRecoveryAccountRequest) (*proto.AuthRecoveryAccountResponse, error) {
+	if len(rq.GetEmail()) == 0 {
+		return nil, status.Error(codes.NotFound, "El campo email esta vacio")
+	}
+
+	response := proto.AuthRecoveryAccountResponse{}
+	response.Success = false
+
+	tokenGenerate := libs.Base64Encode(fmt.Sprintf("%s-%s", rq.GetEmail(), time.Now()))
+
+	exec, err := then.DB.Exec(
+		"UPDATE users set recovery_password=$1 where email=$2",
+		tokenGenerate,
+		rq.GetEmail(),
+	)
+
+	if err != nil {
+		return &response, status.Error(codes.NotFound, "No se ha podido recuperar el usuario")
+	}
+
+	if row, err := exec.RowsAffected(); row == 0 || err != nil {
+		return &response, status.Error(codes.NotFound, "El usuario no existe")
+	}
+
+	response.Success = true
+	return &response, nil
+}
+func (then AuthController) RecoveryAccount(_ context.Context, rq *proto.AuthRecoveryAccountChangeRequest) (*proto.AuthRecoveryAccountResponse, error) {
+	if len(rq.GetEmail()) == 0 {
+		return nil, status.Error(codes.NotFound, "El campo email esta vacio")
+	}
+
+	if len(rq.GetToken()) == 0 {
+		return nil, status.Error(codes.NotFound, "El campo token esta vacio")
+	}
+
+	if len(rq.GetPassword()) == 0 {
+		return nil, status.Error(codes.NotFound, "El campo password esta vacio")
+	}
+	response := proto.AuthRecoveryAccountResponse{}
+	response.Success = false
+
+	// TODO: Mejor buscar el usuario si existe y despues actualizarlo? o hacemos que cuando de error el update...
+
+	exec, err := then.DB.Exec(
+		"UPDATE users set recovery_password=$1, password=$4 where email=$2 and recovery_password=$3",
+		nil,
+		rq.GetEmail(),
+		rq.GetToken(),
+		rq.GetPassword(),
+	)
+
+	if err != nil {
+		return &response, status.Error(codes.NotFound, "No se ha podido recuperar el usuario")
+	}
+
+	if row, err := exec.RowsAffected(); row == 0 || err != nil {
+		return &response, status.Error(codes.NotFound, "El usuario no existe")
+	}
+
+	response.Success = true
 	return &response, nil
 }
